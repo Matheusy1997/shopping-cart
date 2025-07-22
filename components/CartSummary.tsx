08c6a4e82d61b2aeb6bab9dda43898744935dd87
Matheusy1997 <matheusmachado.dev@gmail.com>
2025-07-22 19:04:18 -0300

    layout do summary

---
+++ components/CartSummary.tsx
@@ -0,0 +1,33 @@
+export default function CartSummary() {
+  return (
+    <div>
+      <div className="w-[230px] bg-zinc-200 border border-zinc-300">
+        <h2 className="font-medium p-3 pl-4 border border-zinc-300">
+          Resumo de compra
+        </h2>
+        <div className="text-zinc-500">
+          <div className="flex justify-between px-4 py-1 text-sm">
+            <p>Subtotal</p>
+            <p>R${2450}</p>
+          </div>
+          <div className="flex justify-between px-4 py-1 text-sm">
+            <p>Frete</p>
+            <p>Gratuito</p>
+          </div>
+          <div className="text-[12px] text-center py-2">
+            <p className="text-green-500">
+              <a className="cursor-pointer" href="#">
+                Adicione cupom de desconto +
+              </a>
+            </p>
+          </div>
+          <div className="w-full flex justify-between p-4 text-xl border border-zinc-300">
+            <p>Total</p>
+            <p>R${2450}</p>
+          </div>
+        </div>
+      </div>
+      <button className="w-full bg-green-500 mt-2.5 p-2 cursor-pointer hover:bg-green-600 transition">FINALIZAR COMPRA</button>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
-import Image from "next/image";
+// import Image from "next/image";
 import CartTable from "../../components/CartTable";
+import CartSummary from "../../components/CartSummary";
 
 
 export default function CartPage() {
   return (
     <main className="h-screen w-screen font-sans flex flex-col items-center">
         <h1 className="my-14 text-5xl font-medium">Seu carrinho</h1>
-        <div className="w-full">
+        <div className="flex justify-around w-full">
           <CartTable/>   
+          <CartSummary/>
         </div>
     </main>
   );
